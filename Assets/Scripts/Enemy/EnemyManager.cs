7f84248c79c49adc0e112e820d87264a6acbcfa1
cocoa-dev-1 <hyeonseung1006@gmail.com>
2024-09-25 01:01:24 +0900

    fdasfd

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -12,7 +12,6 @@ public class EnemyManager : MonoBehaviour
 
     private void Awake()
     {
-        Debug.Log("EnemyManager Awake");
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
