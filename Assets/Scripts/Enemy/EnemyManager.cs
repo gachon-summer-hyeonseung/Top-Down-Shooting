ce95aac3b8212a515ab6bb507b8cb196982313df
cocoa-dev-1 <hyeonseung1006@gmail.com>
2024-09-25 00:50:50 +0900

    feat: add debug log for enemy manager

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -12,6 +12,7 @@ public class EnemyManager : MonoBehaviour
 
     private void Awake()
     {
+        Debug.Log("EnemyManager Awake");
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
